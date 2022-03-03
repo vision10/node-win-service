420cc3876705418682f170b2288d36ee86412fb3
vision10 <sorinm025@gmail.com>
2022-03-03 20:01:03 +0200

    service exxamples logs

--- examples/service.js
+++ examples/service.js
@@ -34,20 +34,25 @@ function errorHandler(error) {
 
 function setEvents(srv) {
     srv.forEach(service => {
-        service.on('stop', function () { console.log(service.name + ' event callback stop !') });
+        const name = service.name || service.id;
+        service.on('stop', function (msg) {
+            console.log(name + ' event callback stop !')
+            if (msg) console.log('msg stop', msg)
+        });
         service.on('uninstall', function (msg) {
-            console.log(service.name + ' event callback uninstall !')
-            if (msg) console.log('msg', msg)
+            console.log(name + ' event callback uninstall !')
+            if (msg) console.log('msg uninstall', msg)
         });
         service.on('install', async function (msg) {
-            console.log(service.name + ' event callback install !')
-            if (msg) console.log('msg', msg)
+            console.log(name + ' event callback install !')
+            if (msg) console.log('msg install', msg)
 
             console.log('start service')
             service.start()
         });
-        service.on('start', function () {
-            console.log(service.name + ' event callback start !')
+        service.on('start', function (msg) {
+            console.log(name + ' event callback start !')
+            if (msg) console.log('msg start', msg)
             setTimeout(() => { service.uninstall(null, null, null, true); }, 5000);
         });
     })
